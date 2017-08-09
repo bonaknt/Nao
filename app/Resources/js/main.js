ccb19fe71f65b0548dc3b4c88543890525d0651c
Tawny <tony.malto.simon@gmail.com>
2017-08-09 17:21:13 +0200

    Implémentation suggestions lors rech espèce. Incomplet

---
+++ app/Resources/js/main.js
@@ -0,0 +1,10 @@
+import searchSpecies from './searchspecies/searchspecies';
+
+searchSpecies();
+
+
+// var raw = '<li class="suggestion">{{title}}</li>';
+// var template = Handlebars.compile(raw);
+// var res = template({title: 'Tony'});
+// console.log(res)
+// var res2 = template({title: 'Jean'});
