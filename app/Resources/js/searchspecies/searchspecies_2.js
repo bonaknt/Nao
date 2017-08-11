3e1d11d4b9f7cea46db481541e62ee6659bc887b
Tawny <tony.malto.simon@gmail.com>
2017-08-11 13:18:59 +0200

    système rech dynamique en cours d'implément.

--- app/Resources/js/searchspecies/searchspecies_2.js
+++ app/Resources/js/searchspecies/searchspecies_2.js
@@ -1,160 +1,33 @@
-import events from '../events';
-import $ from 'jquery';
-import birdTemplate from './birdTemplate';
-import difference from 'lodash.difference';
 
-
-export default function main() {
-    console.log('hello Im main');
-    var template = Handlebars.compile(birdTemplate);
-    // print(template({'birds': [{'name':'aaaaaa'}, {'name': 'bbbbbbbb'}]}))
-
-    var inputFormObj = inputForm();
-    var speMod = speciesModel();
-    var speView = speciesView(template);
-
-    events.on('inputChangeEvent', speMod.updateSuggestionsArray);
-    events.on('speciesUpdatedEvent', inputFormObj.updateCurrentSuggestions);
-    events.on('speciesUpdatedEvent', speView.renderSpecies);
-}
-
-// suggestions façon google (un seul block)
-function speciesModel() {
-    var allSpecies = hydrateSpecies();
-    var allPatternMatchingSpecies = [];
-    function uppdateSuggestionsArray(currentString) {
-        allPatternMatchingSpecies = [];
-        var regexString = '^' + currentString ;
-        var regex = new RegExp(regexString, 'i');
-        for (var i = 0; i < allSpecies.length; i++) {
-            if (regex.test(allSpecies[i])) {
-                allPatternMatchingSpecies.push(allSpecies[i]);
-            }
-        }
-        events.emit('speciesUpdatedEvent', allPatternMatchingSpecies);
-    }
-    // makes ajax query and hydrate allSpeciesArray
-    function hydrateSpecies() {
-        //TODO: ajax query
-        //fixte
-        return getAllSpeciesFixture();
-    }
-    return {
-        allCurrentSuggestionsArray: allPatternMatchingSpecies,
-        updateSuggestionsArray: uppdateSuggestionsArray
-    }
-}
-
-
-function speciesView(template) {
-    const birdTemplate = template;
-    const $resultRow = $('#result-row');
-    // to be hydrated just before speciesModel.allPatternMatchingSpecies is updated;
-    // then the difference is made between prev state et new state for optimization reasons for rendering AND DO NOT FORGET TO REMOVE
-    // diff PREV - NEW => remove
-
-    let allPatternMatchingSpeciesPrev = [];
-
-    function renderSpecies(speciesToRender) {
-        $resultRow.empty();
-        function birdObject(birdName) {
-            this.name = birdName;
-        }
-        let birdObjectsArray = [];
-        for (let i = 0; i < speciesToRender.length; i++) {
-            let bird = new birdObject(speciesToRender[i]);
-            birdObjectsArray.push(bird);
-        }
-        let html = birdTemplate({'birds': birdObjectsArray});
-        print(html)
-        $resultRow.append(html);
-    }
-    return {
-        renderSpecies: renderSpecies
-    }
-}
-
-
-function inputForm() {
-    var currentSuggestionsArray = [];
-    var $input = $('#search-input');
-    var $suggestionsContainer = $('#suggestions-container');
-    var currentlyHighlighted = -1;
-
-    //dom events binding
-    $input.on('input', function(e){
-        currentlyHighlighted = -1;
-        var currentValue = $(e.target).val();
-        events.emit('inputChangeEvent', currentValue);
+$( function() {
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $( "search-input" ).autocomplete({
+        source: availableTags
     });
-    $input.on('focusout', function() {
-        $suggestionsContainer.empty();
-        currentlyHighlighted = [];
-    });
-    $(document).on('keydown', function(e) {
-        print('keydown')
-        var keyPressed = String.fromCharCode(e.keyCode);
-        var suggestionsLength =  currentSuggestionsArray.length;
-
-        if (suggestionsLength !== 0) {
-            // UP
-            if (keyPressed == '&') {
-                if (currentlyHighlighted !== 0) {
-                    currentlyHighlighted = currentlyHighlighted - 1;
-                }
-                highlightSuggestion(suggestionsLength)
-                print(currentlyHighlighted)
-            }
-            // DOWN
-            if (keyPressed == '(') {
-                if (currentlyHighlighted < suggestionsLength - 1) {
-                    currentlyHighlighted++;
-                }
-                highlightSuggestion(suggestionsLength)
-                print(currentlyHighlighted)
-            }
-        } else {
-        }
-    });
-
-    function updateCurrentSuggestions(allPatternMAtchingSpecies) {
-        currentSuggestionsArray = allPatternMAtchingSpecies.slice(0,4);
-        print('suggestions updated')
-        renderSuggestions(currentSuggestionsArray);
-    }
-
-    // view functions
-    function renderSuggestions(currentSuggestionsArray) {
-        $suggestionsContainer.empty();
-        for (var i = 0; i < currentSuggestionsArray.length; i++) {
-            var rawSuggestion = currentSuggestionsArray[i];
-            var suggestion = '<li class="suggestion">' + rawSuggestion + '</li>';
-            $(suggestion).appendTo($input)
-            // print(suggestion)
-            $suggestionsContainer.append(suggestion);
-        }
-    }
-    function highlightSuggestion() {
-        // if (index !== -1) {// new highlight
-        //     var $liToHighlight = $suggestionsContainer.find('li').eq(index);
-        //     $liToHighlight.css('background-color', 'blue');
-        // }
-        if (currentlyHighlighted !== -1) {
-            var $liToHighlight = $suggestionsContainer.find('li').eq(currentlyHighlighted).addClass('selected');
-            $liToHighlight.next().removeClass('selected');
-            $liToHighlight.prev().removeClass('selected');
-        }
-    }
-
-    return {
-        inputElement: $input,
-        updateCurrentSuggestions: updateCurrentSuggestions
-    }
-}
-
-
-
-
+} );
 
 
 function getAllSpeciesFixture() {
@@ -571,8 +444,4 @@ function getAllSpeciesFixture() {
         'Zoothera aurea (Holandre, 1825)'];
 
     return species;
-}
-
-function print(string) {
-    console.log(string)
 }
