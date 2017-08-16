03d0b4d7acad2c2feb512c4cd783f4f8afea1f4f
Tawny <tony.malto.simon@gmail.com>
2017-08-16 10:15:02 +0200

    Species : implémententation Serializable

--- app/Resources/js/main.js
+++ app/Resources/js/main.js
@@ -1,15 +1,9 @@
 import searchSpecies from './searchspecies/searchspecies';
 
-searchSpecies();
+if (isLocation('speciessearch')) searchSpecies();
 
-const raw = '<li>{{name}}' +
-    '</li>' +
-    '';
-const template = Handlebars.compile(raw);
-const html = template({name:'John'});
 
-// var raw = '<li class="suggestion">{{title}}</li>';
-// var template = Handlebars.compile(raw);
-// var res = template({title: 'Tony'});
-// console.log(res)
-// var res2 = template({title: 'Jean'});
+function isLocation(location) {
+    return window.location.href.includes(location)
+}
+
