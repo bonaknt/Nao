82143dcba4ef059d59358a723ec1ea99fa138841
Kevin SI <sikevin.sk@gmail.com>
2017-08-20 12:20:07 +0200

    création du formulaire de contact

--- src/AppBundle/Controller/UsefulLinkController.php
+++ src/AppBundle/Controller/UsefulLinkController.php
@@ -6,6 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class UsefulLinkController extends Controller
 {
 	/**
@@ -13,7 +17,29 @@ class UsefulLinkController extends Controller
 	 */
 	public function contactUsAction(Request $request)
 	{
-		return $this->render('nao/usefulLinks/contactUs.html.twig');
+
+		$contactForm = $this->createFormBuilder()
+			->add('name', TextType::class, array(
+				'label'	=>	"Nom complet",
+				'attr'	=>	['class'	=>	'form-control']
+			))
+			->add('obj', TextType::class, array(
+				'label'	=>	"Objet",
+				'attr'	=>	['class'	=>	'form-control']
+			))
+			->add('msg', TextareaType::class, array(
+				'label'	=>	"Message",
+				'attr'	=>	['class'	=>	'form-control', 'style'	=>	'resize:vertical; min-height:200px;']
+			))
+			->add('send', SubmitType::class, array(
+				'label' => 'Envoyer',
+				'attr'	=>	['class'	=>	'btn btn-primary']
+			))
+			->getForm();
+
+		return $this->render('nao/usefulLinks/contactUs.html.twig', array(
+			'contactForm'	=> $contactForm->createView(),
+		));
 	}
 
 	/**
