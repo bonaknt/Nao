1ddf296b83d51cb2f14afc35ed57e077ce09be3b
Tawny <tony.malto.simon@gmail.com>
2017-08-24 12:59:29 +0200

    initmap ok

---
+++ web/static/initMap.js
@@ -0,0 +1,22 @@
+var $latInput = $('.latitude');
+var $lngInput = $('.longitude');
+function initMap() {
+        var paris = {lat: 48.864716, lng: 2.349014};
+        var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 5,
+                center: paris
+        });
+
+            var obs;
+        map.addListener('click', function(e) {
+                var lat = e.latLng.lat();
+                var lng = e.latLng.lng();
+                $latInput.val(lat);
+                $lngInput.val(lng);
+                if (obs) obs.setMap(null);
+                obs = new google.maps.Marker({
+                        position: {lat: lat, lng: lng},
+                    map: map
+                });
+            })
+    }
