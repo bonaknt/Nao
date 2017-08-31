adcf570faa0faa52339a2f56b57c9cdec1f7b33f
Kevin SI <sikevin.sk@gmail.com>
2017-08-31 15:48:52 +0200

    création de l'entité newsletter

---
+++ src/AppBundle/Entity/Newsletter.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Newsletter
+ *
+ * @ORM\Table(name="newsletter")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NewsletterRepository")
+ */
+class Newsletter
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="validity", type="boolean")
+     */
+    private $validity;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Newsletter
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set validity
+     *
+     * @param boolean $validity
+     *
+     * @return Newsletter
+     */
+    public function setValidity($validity)
+    {
+        $this->validity = $validity;
+
+        return $this;
+    }
+
+    /**
+     * Get validity
+     *
+     * @return bool
+     */
+    public function getValidity()
+    {
+        return $this->validity;
+    }
+}
+
